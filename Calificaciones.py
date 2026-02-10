54d007fee0ed29da398a542d0770d20294aa8b26
Arath_PM-092 <arathpm66@gmail.com>
2026-02-09 23:41:31 -0600

    Delete Calificaciones.py

--- Calificaciones.py
+++
@@ -1,9 +0,0 @@
-calificaciones = [0] * 5
-
-for i in range(5):
-    calificaciones[i] = int(input(f"Captura la calificación {i + 1}: "))
-
-print("Calificaciones capturadas:")
-for cal in calificaciones:
-
-    print(cal)
