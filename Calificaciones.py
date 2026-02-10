f07b1f58d42e1d163c77aa9c5d43a1c29e1ac8a2
arathpm66-boop <arathpm66@gmail.com>
2026-02-09 18:02:06 -0600

    Calificaciones.py

--- Calificaciones.py
+++ Calificaciones.py
@@ -3,7 +3,7 @@ calificaciones = [0] * 5
 for i in range(5):
     calificaciones[i] = int(input(f"Captura la calificación {i + 1}: "))
 
-# Mostrar resultados
 print("Calificaciones capturadas:")
 for cal in calificaciones:
+
     print(cal)
